d3c6e5e33c7827386c4ec280c583fa3e8da5ef23
amit <amit.nativ@elbitsystems.com>
2020-09-07 13:54:39 +0300

    fixing minor bug

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 # downloading databases
 # train
-python3 main.py downloader -y --Dataset /home/amit/Data/OID --multiclasses 1 --type_csv train --classes Window Door Building --image_IsGroupOf 0 --image_IsDepiction 0 --image_IsInside 0
+python3 main.py downloader -y --Dataset /home/amit/Data/OID --multiclasses 1 --type_csv train --classes Window Person Car --image_IsGroupOf 0 --image_IsDepiction 0 --image_IsInside 0 --limit 1000
 
 # validation
-python3 main.py downloader -y --Dataset /home/amit/Data/OID --multiclasses 1 --type_csv validation --classes Window Door Building  --image_IsGroupOf 0 --image_IsDepiction 0 --image_IsInside 0
+python3 main.py downloader -y --Dataset /home/amit/Data/OID --multiclasses 1 --type_csv validation --classes Window Person Car  --image_IsGroupOf 0 --image_IsDepiction 0 --image_IsInside 0 --limit 1000
